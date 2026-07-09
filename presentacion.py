13ca17c45645653caeadc0ef1aa00080737547c1
Sara Martínez <saramartinezpr11@gmail.com>
2026-07-09 15:01:04 -0500

    Reto 1 – Mi primera presentación en GitHub

---
+++ presentacion.py
@@ -0,0 +1,12 @@
+# presentacion.py
+# Mi primera contribución a GitHub
+nombre = "Sara Sofía Martínez Quiñonez"
+grado = "11°"
+colegio = "SENA"
+lenguaje = "Python 🐍"
+print("=" * 45)
+print(f" Hola GitHub! Soy {nombre}")
+print(f" Grado: {grado} | Institución: {colegio}")
+print(f" Aprendiendo: {lenguaje}")
+print(f" ¡Este es mi primer commit! ")
+print("=" * 45)
